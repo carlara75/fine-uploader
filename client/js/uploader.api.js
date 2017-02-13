23b6b997c1b4c63bd7aded716c0f0d4adcebe65d
Nicholas Albion <nalbion@yahoo.com>
2017-02-14 09:50:24 +1100

    removed `uuid` parameter for consistency

--- client/js/uploader.api.js
+++ client/js/uploader.api.js
@@ -523,7 +523,7 @@
                 self = this,
                 retVal;
 
-            retVal = this._options.showConfirm(confirmMessage, id, uuid);
+            retVal = this._options.showConfirm(confirmMessage, id);
 
             if (qq.isGenericPromise(retVal)) {
                 retVal.then(function() {
